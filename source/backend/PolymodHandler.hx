14b538f4572f36cf66856be256ce8e17b1d0df06
Joalor64 <rhgarcon@gmail.com>
2025-05-10 16:16:20 -0400

    Update PolymodHandler.hx

--- source/backend/PolymodHandler.hx
+++ source/backend/PolymodHandler.hx
@@ -3,22 +3,32 @@ package backend;
 #if FUTURE_POLYMOD
 import polymod.Polymod;
 import polymod.backends.PolymodAssets.PolymodAssetType;
+import polymod.fs.PolymodFileSystem;
 import polymod.format.ParseRules;
 #end
 
-class PolymodHandler {
+class ModHandler {
 	static final MOD_DIR:String = 'mods';
+	static final GLOBAL_MOD_ID:String = 'global';
 	static final CORE_DIR:String = 'assets';
+
 	static final API_VERSION:String = '1.0.0';
+	static final API_VERSION_MATCH:String = '*.*.*';
+
+	static var fs(default, null):IFileSystem;
 
 	#if FUTURE_POLYMOD
 	private static final extensions:Map<String, PolymodAssetType> = [
 		'ogg' => AUDIO_GENERIC,
+		'wav' => AUDIO_GENERIC,
 		'png' => IMAGE,
-        'jpg' => IMAGE,
 		'xml' => TEXT,
 		'json' => TEXT,
 		'txt' => TEXT,
+		'hx' => TEXT,
+		'hxs' => TEXT,
+		'hxc' => TEXT,
+		'hscript' => TEXT,
 		'ttf' => FONT,
 		'otf' => FONT
 	];
@@ -29,10 +39,13 @@ class PolymodHandler {
 	public static function reload():Void {
 		#if FUTURE_POLYMOD
 		trace('Reloading Polymod...');
-        if (!FileSystem.exists(MOD_DIR))
-			FileSystem.createDirectory(MOD_DIR);
-		if (!FileSystem.exists(MOD_DIR + '/mods-go-here.txt'))
-			File.saveContent(MOD_DIR + '/mods-go-here.txt', '');
+		if (!FileSystem.exists('./mods/'))
+			FileSystem.createDirectory('./mods/');
+		if (!FileSystem.exists('mods/mods-go-here.txt'))
+			File.saveContent('mods/mods-go-here.txt', '');
+
+		fs = PolymodFileSystem.makeFileSystem(null, {modRoot: MOD_DIR});
+
 		loadMods(getMods());
 		#else
 		trace("Polymod reloading is not supported on your Platform!");
@@ -67,19 +80,18 @@ class PolymodHandler {
 	public static function getMods():Array<String> {
 		trackedMods = [];
 
-		if (FlxG.save.data.disabledMods == null) {
-			FlxG.save.data.disabledMods = [];
-			FlxG.save.flush();
-		}
-
 		var daList:Array<String> = [];
+		var globalDirPath:String = '$MOD_DIR/$GLOBAL_MOD_ID';
+		if (fs.exists(globalDirPath) && fs.isDirectory(globalDirPath))
+			daList.push(GLOBAL_MOD_ID);
 
 		trace('Searching for Mods...');
 
-		for (i in Polymod.scan({modRoot: MOD_DIR, apiVersionRule: API_VERSION, errorCallback: onError})) {
-			trackedMods.push(i);
-			if (!FlxG.save.data.disabledMods.contains(i.id))
+		for (i in Polymod.scan({modRoot: MOD_DIR, apiVersionRule: API_VERSION_MATCH, errorCallback: onError})) {
+			if (i != null) {
+				trackedMods.push(i);
 				daList.push(i.id);
+			}
 		}
 
 		if (daList != null && daList.length > 0)
@@ -95,6 +107,11 @@ class PolymodHandler {
 	public static function getParseRules():ParseRules {
 		final output:ParseRules = ParseRules.getDefault();
 		output.addType("txt", TextFileFormat.LINES);
+		output.addType("json", TextFileFormat.JSON);
+		output.addType("hx", TextFileFormat.PLAINTEXT);
+		output.addType("hxs", TextFileFormat.PLAINTEXT);
+		output.addType("hxc", TextFileFormat.PLAINTEXT);
+		output.addType("hscript", TextFileFormat.PLAINTEXT);
 		return output != null ? output : null;
 	}
 
